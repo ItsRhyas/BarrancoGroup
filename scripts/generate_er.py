e5b6a4b403a1fe7ff7d6b27223c7f9d628887347
Adrián David Laínez Hernández <adriandavidlainezhernandez@gmail.com>
2026-08-30 15:14:33 -0600

    docs: update DB model with session-user relationship

--- scripts/generate_er.py
+++ scripts/generate_er.py
@@ -24,12 +24,12 @@ digraph MairinER {
 
   users [label=<<table border="0" cellborder="1" cellspacing="0" cellpadding="6">
     <tr><td bgcolor="#805ad5" width="170"><font color="white"><b>USER</b></font></td></tr>
-    <tr><td align="left">PK id<br/>username UQ<br/>passwordHash<br/>role<br/>createdAt</td></tr>
+    <tr><td align="left">PK id<br/>username UQ?<br/>passwordHash?<br/>role<br/>createdAt</td></tr>
   </table>>];
 
   game_sessions [label=<<table border="0" cellborder="1" cellspacing="0" cellpadding="6">
     <tr><td bgcolor="#2f855a" width="170"><font color="white"><b>GAME_SESSION</b></font></td></tr>
-    <tr><td align="left">PK id<br/>sessionToken UQ<br/>createdAt</td></tr>
+    <tr><td align="left">PK id<br/>FK userId<br/>sessionToken UQ<br/>createdAt</td></tr>
   </table>>];
 
   attempts [label=<<table border="0" cellborder="1" cellspacing="0" cellpadding="6">
@@ -37,6 +37,7 @@ digraph MairinER {
     <tr><td align="left">PK id<br/>FK sessionId<br/>levelId<br/>success<br/>endingId?<br/>attemptNumber<br/>completedAt</td></tr>
   </table>>];
 
+  users -> game_sessions [xlabel="1..N"];
   game_sessions -> attempts [xlabel="1..N"];
 }
 """
